a58ce30a503622397d221a078be3a2bd8040df65
Hepestoo <teckalan2@gmail.com>
2025-11-21 09:16:22 -0500

    listo15

--- src/app/admin/inventario/components/categoria/categoria.component.ts
+++ src/app/admin/inventario/components/categoria/categoria.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CategoriaService } from '../../services/categoria.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 interface Categoria {
   id?: number;
@@ -10,18 +11,17 @@ interface Categoria {
 @Component({
   selector: 'app-categoria',
   templateUrl: './categoria.component.html',
-  styleUrl: './categoria.component.scss'
+  styleUrl: './categoria.component.scss',
 })
 export class CategoriaComponent implements OnInit {
-guardarCategoria() {
-throw new Error('Method not implemented.');
-}
-
   private categoriaService = inject(CategoriaService);
 
   categorias: Categoria[] = [];
-  visible:boolean=false
-dialog_visible: any;
+  visible: boolean = false;
+  categoriaForm = new FormGroup({
+    nombre: new FormControl(''),
+    detalle: new FormControl(''),
+  });
 
   ngOnInit(): void {
     this.getCategorias();
@@ -37,11 +37,8 @@ dialog_visible: any;
       }
     );
   }
-  mostrarDialog(){
-    this.visible=true
-
+  mostrarDialog() {
+    this.visible = true;
   }
-  guadarCategoria(){
-    
-  }
+  guardarCategoria() {}
 }
--- src/app/admin/inventario/inventario.module.ts
+++ src/app/admin/inventario/inventario.module.ts
@@ -8,6 +8,7 @@ import { TableModule } from 'primeng/table';
 import { DialogModule } from 'primeng/dialog';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -22,7 +23,8 @@ import { InputTextModule } from 'primeng/inputtext';
     TableModule,
     DialogModule,
     ButtonModule,
-    InputTextModule
+    InputTextModule,
+    ReactiveFormsModule
 ]
 })
 export class InventarioModule { }
